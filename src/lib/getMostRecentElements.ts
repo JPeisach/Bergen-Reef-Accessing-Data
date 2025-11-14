5dba2a2ba49b6d1b9b994268bc41699bb49250e1
Joshua Peisach <itzswirlz2020@outlook.com>
2025-11-14 11:08:24 -0500

    Fix next build errors in DB get functions

--- src/lib/getMostRecentElements.ts
+++ src/lib/getMostRecentElements.ts
@@ -1,29 +1,37 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/mysql2";
 import { eq, desc } from "drizzle-orm";
-import { dataTable } from "src/db/data-schema";
+import { coralData } from "src/db/schema";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
 export default async function getMostRecentElements() {
   try {
-    const elements = ['pH', 'Salinity', 'Temperature', 'ORP', 'Alkalinity', 'Calcium', 'Nitrate', 'Nitrite', 'Phosphate'];
+    const elements = [
+      "pH",
+      "Salinity",
+      "Temperature",
+      "ORP",
+      "Alkalinity",
+      "Calcium",
+      "Nitrate",
+      "Nitrite",
+      "Phosphate",
+    ];
     let values = [];
 
     for (let i = 0; i < elements.length; i++) {
-        var result = await db
+      var result = await db
         .select()
-        .from(dataTable)
-        .where(eq(dataTable.name, elements[i]))
-        .orderBy(desc(dataTable.datetime))
+        .from(coralData)
+        .orderBy(desc(coralData.timestamp))
         .limit(1);
 
-        values.push(result[0]);
+      values.push(result[0]);
     }
 
     return values;
-  } 
-  catch (error) {
+  } catch (error) {
     console.error("Error fetching most recent elements:", error);
     throw new Error("Failed to fetch most recent elements.");
   }
