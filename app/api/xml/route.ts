0d58cf17a6d586a2b0bfca5c2a545ad63cb0cc4f
Joshua Peisach <itzswirlz2020@outlook.com>
2025-11-19 14:45:40 -0500

    api/xml: add debug logging for xml uploads

--- app/api/xml/route.ts
+++ app/api/xml/route.ts
@@ -6,11 +6,13 @@ export async function POST(request: Request) {
   try {
     // Get the XML file within the request
     const raw_xml = await request.text();
+    console.log("Raw XML:", raw_xml);
 
     // Use xml2js package to convert XML into JSON
     const parser = new xml2js.Parser({ explicitArray: false });
     const parsed_xml = await parser.parseStringPromise(raw_xml);
 
+    console.log("Parsed XML:", parsed_xml);
     await insertData(parsed_xml.status.probes, parsed_xml.status.date);
 
     return NextResponse.json({ status: 200 });
