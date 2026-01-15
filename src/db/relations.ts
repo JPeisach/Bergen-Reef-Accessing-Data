2c4bc1283b21e26633e34e1673ae8a5f802ac0c8
Joshua Peisach (ItzSwirlz) <itzswirlz2020@outlook.com>
2026-01-15 10:25:15 -0500

    Update DB schema (#30)

--- src/db/relations.ts
+++ src/db/relations.ts
@@ -1,10 +1,5 @@
 import { relations } from "drizzle-orm/relations";
-import {
-  coralData,
-  coralDataObservationsTable,
-  observations,
-  accounts,
-} from "./schema";
+import { coralData, coralDataObservationsTable, observations } from "./schema";
 
 export const coralDataObservationsTableRelations = relations(
   coralDataObservationsTable,
@@ -22,19 +17,4 @@ export const coralDataObservationsTableRelations = relations(
 
 export const coralDataRelations = relations(coralData, ({ many }) => ({
   coralDataObservationsTables: many(coralDataObservationsTable),
-}));
-
-export const observationsRelations = relations(
-  observations,
-  ({ one, many }) => ({
-    coralDataObservationsTables: many(coralDataObservationsTable),
-    account: one(accounts, {
-      fields: [observations.authorId],
-      references: [accounts.accountId],
-    }),
-  }),
-);
-
-export const accountsRelations = relations(accounts, ({ many }) => ({
-  observations: many(observations),
 }));
