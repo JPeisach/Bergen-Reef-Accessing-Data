648eb8f10f18450ed938a344189dbf6877392c15
jolene <123101131+jolene12345@users.noreply.github.com>
2026-01-27 07:33:44 -0500

    notis page (#40)

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -1,10 +1,12 @@
 import { Tab, TabGroup, TabList } from "@headlessui/react";
-import { UserCircleIcon, Cog6ToothIcon } from "@heroicons/react/24/solid";
+import { UserCircleIcon, Cog6ToothIcon, BellIcon } from "@heroicons/react/24/solid";
+
 import clsx from "clsx";
 import { Fragment } from "react";
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+
 
 interface NavigationBarProps {
   defaultIndex: number;
@@ -198,14 +200,24 @@ const NavigationBar: React.FC<NavigationBarProps> = ({
             </TabList>
           </TabGroup>
         </div>
+        
         <div className="mt-auto">
           <a href="/settings">
-            <button className="tab-item w-full py-5 transition flex items-center justify-center gap-2 bg-light-gray outline outline-1 outline-medium-gray drop-shadow-xl text-gray font-semibold hover:bg-medium-orange">
+            <button className="tab-item w-full py-5 transition flex items-center justify-bottom gap-2 bg-light-gray outline outline-1 outline-medium-gray drop-shadow-xl text-gray font-semibold hover:bg-medium-orange">
               <Cog6ToothIcon className="size-5" />
               Settings
             </button>
           </a>
         </div>
+        <div className="mt">
+          <a href="/notifications">
+            <button className="tab-item py-5 transition flex items-center justify-top gap-0 bg-light-gray outline outline-1 outline-medium-gray drop-shadow-xl text-gray font-semibold hover:bg-medium-orange">
+              <BellIcon className="size-5" /> 
+                Notifications
+            </button>
+          </a>
+        </div>
+        
       </div>
     </>
   );
