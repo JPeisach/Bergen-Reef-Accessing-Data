13947fe0a64aea42ef7386ed82bc55d0ae93578c
Joshua Peisach <itzswirlz2020@outlook.com>
2026-04-28 10:50:55 -0400

    RecentDataTankBox: warn on old data, error on no data
    Signed-off-by: Joshua Peisach <itzswirlz2020@outlook.com>

--- app/components/tankBoxes/RecentDataTankBox.tsx
+++ app/components/tankBoxes/RecentDataTankBox.tsx
@@ -14,6 +14,12 @@ import { useEffect, useState } from "react";
 export default function RecentDataTankBox({ tankName, variableType }) {
   const [chartData, setChartData] = useState([]);
 
+  const isDataOld = () => {
+    if (chartData.length === 0) return false;
+    const lastDatetime = new Date(chartData[chartData.length - 1].datetime);
+    return new Date().getTime() - lastDatetime.getTime() > 604800 * 1000;
+  };
+
   useEffect(() => {
     async function fetchData() {
       try {
@@ -58,49 +64,60 @@ export default function RecentDataTankBox({ tankName, variableType }) {
       <h2 className="text-xl font-bold text-primary text-center">
         Tank {tankName}
       </h2>
-      {/* FIXME: we can't use dvh, needs to be precalculated. */}
-      <div className="h-[80%] w-full">
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart data={chartData} margin={{ left: -10 }}>
-            <CartesianGrid strokeDasharray="3 3" stroke="#E5E7EB" />
-            <XAxis
-              dataKey="datetime"
-              tickFormatter={(tick) =>
-                tick.split("/")[0] + "/" + tick.split("/")[1]
-              }
-              stroke="#757575"
-              fontSize={12}
-            />
-            <YAxis
-              domain={["dataMin - 1", "dataMax + 1"]}
-              tickFormatter={(tick) => tick.toFixed(1).toString()}
-              stroke="#757575"
-              scale="sequential"
-              fontSize={12}
-            />
-            <Tooltip
-              contentStyle={{
-                backgroundColor: "var(--color-base-content)",
-                border: "1px solid var(--color-base-content)",
-                borderRadius: "8px",
-              }}
-              itemStyle={{
-                color: "var(--base-text-content)",
-              }}
-            />
-            <Area
-              type="monotone"
-              dataKey="value"
-              stroke="var(--color-primary)"
-              dot={false}
-              fill="var(--color-primary)"
-              fillOpacity={0.5}
-              strokeWidth={2.5}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
-      <DownloadCSVButton data={chartData}></DownloadCSVButton>
+      {chartData.length != 0 ? (
+        <div className="h-[80%] w-full">
+          {isDataOld() ? (
+            <p className="text-xs font-bold text-warning text-center w-full">
+              Warning: Recent data is older than one week. This tank may be
+              offline.
+            </p>
+          ) : null}
+          <ResponsiveContainer width="100%" height="100%">
+            <AreaChart data={chartData} margin={{ left: -10 }}>
+              <CartesianGrid strokeDasharray="3 3" stroke="#E5E7EB" />
+              <XAxis
+                dataKey="datetime"
+                tickFormatter={(tick) =>
+                  tick.split("/")[0] + "/" + tick.split("/")[1]
+                }
+                stroke="#757575"
+                fontSize={12}
+              />
+              <YAxis
+                domain={["dataMin - 1", "dataMax + 1"]}
+                tickFormatter={(tick) => tick.toFixed(1).toString()}
+                stroke="#757575"
+                scale="sequential"
+                fontSize={12}
+              />
+              <Tooltip
+                contentStyle={{
+                  backgroundColor: "var(--color-base-content)",
+                  border: "1px solid var(--color-base-content)",
+                  borderRadius: "8px",
+                }}
+                itemStyle={{
+                  color: "var(--base-text-content)",
+                }}
+              />
+              <Area
+                type="monotone"
+                dataKey="value"
+                stroke="var(--color-primary)"
+                dot={false}
+                fill="var(--color-primary)"
+                fillOpacity={0.5}
+                strokeWidth={2.5}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+          <DownloadCSVButton data={chartData}></DownloadCSVButton>
+        </div>
+      ) : (
+        <p className="mt-[25%] text-xl font-bold text-error text-center">
+          This tank has no data. It may be offline.
+        </p>
+      )}
     </div>
   );
 }
